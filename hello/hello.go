8997391c394ac11b5486e031ee1f759dcdf5b3b5
huangyao <505757398@163.com>
2019-10-11 21:23:23 +0800

    hello done

--- hello/hello.go
+++ hello/hello.go
@@ -7,14 +7,30 @@ import "fmt"
 //}
 
 const helloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return helloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+
+	switch language {
+	case "french":
+		prefix = frenchHelloPrefix
+	case "spanish":
+		prefix = spanishHelloPrefix
+	default:
+		prefix = helloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
