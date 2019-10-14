1c9616b706b7731ccde092403989abb6c4d3a390
huangyao <505757398@163.com>
2019-10-14 10:46:52 +0800

    test

--- arrays/sum.go
+++ arrays/sum.go
@@ -10,8 +10,14 @@ package main
 // }
 
 // Sum calc sum using range
-func Sum(numbers [5]int) int {
+// 注意，重构之后，需要维护测试代码
+/*
+质疑测试的价值是非常重要的。测试不是越多越好，而是要尽可能的使你的代码更加健壮
+太多的测试会增加维护成本，因为维护每个测试都是需要成本的
+*/
+func Sum(numbers []int) int {
 	sum := 0
+	// index and value
 	for _, number := range numbers {
 		sum += number
 	}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -4,14 +4,28 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	// 数组容量是我们在声明它们时指定的固定值
-	// 初始化时，[] 内是数字则是显示指定容量，是 ... 则不指定容量，让编译器自行根据初始列表计算容量
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		// 数组容量是我们在声明它们时指定的固定值
+		// 初始化时，[] 内是数字则是显示指定容量，是 ... 则不指定容量，让编译器自行根据初始列表计算容量
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	if want != got {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		// no
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+
+	})
 }
