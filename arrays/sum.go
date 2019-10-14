83c8cf5669697c828d69c33bf44b963f082c2c1c
huangyao <505757398@163.com>
2019-10-14 10:37:29 +0800

    sum using range

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,9 +1,19 @@
 package main
 
+// // Sum calc sum
+// func Sum(numbers [5]int) int {
+// 	sum := 0
+// 	for i := 0; i < 5; i++ {
+// 		sum += numbers[i]
+// 	}
+// 	return sum
+// }
+
+// Sum calc sum using range
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
