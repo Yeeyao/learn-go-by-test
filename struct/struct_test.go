18b577f5e2c28c3a4bab9d0de8e6b2d95603d41e
huangyao <505757398@163.com>
2019-10-14 19:16:15 +0800

    interface

--- struct/struct_test.go
+++ struct/struct_test.go
@@ -18,6 +18,16 @@ func(receiverName ReceiverType) MethodName(args)
 Go 这种静态类型语言中的一种非常强有力的概念，
 因为接口可以让函数接受不同类型的参数并能够创造类型安全且高解耦的代码
 
+接口指定了一个方法集合
+一个接口类型的变量可以存储任何含有接口方法集合的超集的类型的一个值
+
+在 Go 语言中，interface resolution 是隐式的，如果传入的类型匹配接口的需要，则编译正确
+
+这里的例子
+
+Rectangle 有一个返回值类型为 float64 的方法 Area，所以它满足接口 Shape
+Circle 有一个返回值类型为 float64 的方法 Area，所以它满足接口 Shape
+
 */
 
 func TestPerimeter(t *testing.T) {
@@ -32,23 +42,22 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got: %.2f want: %.2f", got, want)
+		}
+	}
+
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{12, 6}
-		got := rectangle.Area(rectangle)
-		want := 72.0
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+		checkArea(t, rectangle, 72.0)
 	})
 
 	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		got := circle.Area(circle)
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+		circles := Circle{10}
+		checkArea(t, circles, 314.1592653589793)
 	})
+
 }
