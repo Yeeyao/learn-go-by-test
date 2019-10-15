c229f2ec0cf3c344f8f8392d65059536ae767ffb
huangyao <505757398@163.com>
2019-10-15 22:02:01 +0800

    self defined type

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,12 +3,9 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	// [] 内是 key 类型，后面跟着的是 value 类型
-	// 这里 key 类型必须是可比较类型
-	// 通过 map[key] 获取 map 的值
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
+	got := dictionary.Search("test")
 	want := "this is just a test"
 
 	assertStrings(t, got, want)
