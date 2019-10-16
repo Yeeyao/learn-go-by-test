04b27d54b9b72b8d548ec2d88c4772563b43ca47
huangyao <505757398@163.com>
2019-10-16 09:56:59 +0800

    maps 2

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,11 +1,17 @@
 package main
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
-}
+// 把错误提取为变量，避免 magic error
+var ErrNotFound = errors.New("could not find the word you were looking for")
 
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
 }
