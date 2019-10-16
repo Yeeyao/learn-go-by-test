fd017d9f2f0fc7ac2c498daf374fbde1742e2a6d
huangyao <505757398@163.com>
2019-10-16 19:48:31 +0800

    update first

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,11 +1,20 @@
 package main
 
-import "errors"
-
 type Dictionary map[string]string
 
 // 把错误提取为变量，避免 magic error
-var ErrNotFound = errors.New("could not find the word you were looking for")
+// 使用 constant error，类似宏定义？
+
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -14,4 +23,28 @@ func (d Dictionary) Search(word string) (string, error) {
 	}
 
 	return definition, nil
+}
+
+// 添加元素
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	// 找不到，赋值
+	case ErrNotFound:
+		d[word] = definition
+		// 已经存在了，提示错误
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+
+	// d[word] = definition
+	// return nil
+}
+
+func (d Dictionary) Update(word, definition string) {
+	d[word] = definition
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -1,6 +1,16 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
+
+/*
+	Map的特性，不使用指针传递就可以修改它们。因为 map 是引用类型。它拥有对底层数据结构的引用，就像指针一样
+	它的底层结构是 hash table 或者 hash map
+	Map 的引用类型，意味着，无论 map 有多大，都只会有一个副本
+
+	初始化 map 直接使用 map 或者使用 make 关键字
+*/
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
@@ -22,6 +32,42 @@ func TestSearch(t *testing.T) {
 
 }
 
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+
+	})
+
+}
+
+func TestUpdate(t *testing.T) {
+	word := "test"
+	definition := "this is just a test"
+	dictionary := Dictionary{word: definition}
+	newDefinition := "new definition"
+
+	dictionary.Update(word, newDefinition)
+
+	assertDefinition(t, dictionary, word, newDefinition)
+}
+
 func assertStrings(t *testing.T, got, want string) {
 	t.Helper()
 	if got != want {
@@ -30,9 +76,21 @@ func assertStrings(t *testing.T, got, want string) {
 }
 
 func assertError(t *testing.T, got, want error) {
-
 	t.Helper()
 	if got != want {
 		t.Errorf("got '%s' want '%s'", got, want)
+	}
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if definition != got {
+		t.Errorf("got '%s' want '%s'", got, definition)
 	}
 }
