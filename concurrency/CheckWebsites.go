a1a3ba18f50cc3bd2f23ef74115e2c2195f92ff2
huangyao <505757398@163.com>
2019-10-18 20:48:02 +0800

    racer

--- concurrency/CheckWebsites.go
+++ concurrency/CheckWebsites.go
@@ -1,21 +1,37 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	// chan result 是 channel 类型的 -- result 的 channel
+	resultChannel := make(chan result)
 
-	// 这里循环每次迭代都会启动一个新的 goroutine，与当前进程（ WebsiteChecker 函数）同时发生，
-	// 每个循环都会将结果添加到 results map 中
+	/*
+		这里循环每次迭代都会启动一个新的 goroutine，与当前进程（ WebsiteChecker 函数）同时发生，
+		每个循环都会将结果添加到 results map 中
+		现在，我们迭代 urls 时，不是直接写入 map，而是使用 send statement 将每个
+		调用 wc 的 result 结构体发送到 resultChannel。这里使用 <- 操作符，
+		channel 放在左边，值放在右边
+	*/
+
 	for _, url := range urls {
 		go func(u string) {
-			results[u] = wc(u)
+			resultChannel <- result{u, wc(u)}
 		}(url)
 	}
 
-	time.Sleep(2 * time.Second)
+	// 这个 for 循环为每个 url 迭代一次，使用 receive expression，它将从通道
+	// 接收到的值分配给变量，使用 <- 操作符
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel
+		results[result.string] = result.bool
+	}
 
 	return results
 }
--- concurrency/CheckWebsites_test.go
+++ concurrency/CheckWebsites_test.go
@@ -6,12 +6,18 @@ import (
 )
 
 /*
+
 Go 中不会阻塞的操作将在称为 goroutine 的单独的进程中进行。
 
 匿名函数，可以在声明的同时执行，匿名函数末尾 () 来实现。维护对其所定义的词汇作用域的访问权
 
 channels
 一个 Go 数据结构，可以同时接收和发送值。这些操作以及细节允许不同进程间的通信
+组织和控制不同进程间的交流，避免 race condition
+
+go test -race 检查是否存在 race condition
+
+运行是通过测试，正确是重构代码，快速是优化代码以使其快速运行。要先运行正确才做优化
 
 */
 
@@ -29,23 +35,16 @@ func TestCheckWebsites(t *testing.T) {
 		"waat://furhurterwe.geds",
 	}
 
-	actualResults := CheckWebsites(mockWebsiteChecker, websites)
-
-	want := len(websites)
-	got := len(actualResults)
-
-	if want != got {
-		t.Fatalf("Wanted %v, got %v", want, got)
-	}
-
-	expectedResults := map[string]bool{
+	want := map[string]bool{
 
 		"http://google.com":          true,
 		"http://blog.gypsydave5.com": true,
 		"waat://furhurterwe.geds":    false,
 	}
 
-	if !reflect.DeepEqual(expectedResults, actualResults) {
-		t.Fatalf("Wanted %v, got %v", expectedResults, actualResults)
+	got := CheckWebsites(mockWebsiteChecker, websites)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("Wanted %v, got %v", want, got)
 	}
 }
