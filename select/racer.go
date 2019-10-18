fa170bf71c222c9353a55346d0b55da6522ac6d4
huangyao <505757398@163.com>
2019-10-18 21:18:51 +0800

    racer test 3

--- select/racer.go
+++ select/racer.go
@@ -6,16 +6,18 @@ import (
 )
 
 func Racer(a, b string) (winner string) {
-	startA := time.Now()
-	http.Get(a)
-	aDuration := time.Since(startA)
 
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
+	aDuartion := measureResponseTime(a)
+	bDuartion := measureResponseTime(b)
 
-	if aDuration < bDuration {
+	if aDuartion < bDuartion {
 		return a
 	}
 	return b
+}
+
+func measureResponseTime(url string) time.Duration{
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
 }
