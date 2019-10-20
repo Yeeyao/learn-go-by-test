f5d65fc508dffa7e375446118d2ffeb75fc6b4f4
huangyao <505757398@163.com>
2019-10-20 10:58:09 +0800

    reflection map

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -6,28 +6,30 @@ func walk(x interface{}, fn func(input string)) {
 
 	val := getValue(x)
 
-	if val.Kind() == reflect.Slice {
-		for i := 0; i < val.Len(); i++ {
-			walk(val.Index(i).Interface(), fn)
-		}
-		return
-	}
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
+	numberOfValues := 0
+	var getField func(int) reflect.Value
 
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			walk(field.Interface(), fn)
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		numberOfValues = val.NumField()
+		getField = val.Field
+	case reflect.Slice, reflect.Array:
+		numberOfValues = val.Len()
+		getField = val.Index
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walk(val.MapIndex(key).Interface(), fn)
 		}
+	}
+	for i := 0; i < numberOfValues; i++ {
+		walk(getField(i).Interface(), fn)
 	}
 }
 
 func getValue(x interface{}) reflect.Value {
-
 	val := reflect.ValueOf(x)
-
 	if val.Kind() == reflect.Ptr {
 		val = val.Elem()
 	}
