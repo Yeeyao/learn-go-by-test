c2dd9c401159b829007189c91636fd8344d4d203
huangyao <505757398@163.com>
2019-10-22 09:50:27 +0800

    io v2

--- io/server_test.go
+++ io/server_test.go
@@ -1,1 +1,48 @@
 package main
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestFileSystemStore(t *testing.T) {
+	t.Run("/league from a reader", func(t *testing.T) {
+		database := strings.NewReader(`[
+		{ "Name": "Cleo", "Wins": 10 },
+		{ "Name": "Chris", "Wins": 33 }]`)
+
+		store := FileSystemStore{database}
+
+		got := store.GetPlayerScore("Chris")
+
+		want := 33
+
+		assertScoreEquals(t, got, want)
+
+	})
+
+	t.Run("/get player socre", func(t *testing.T) {
+		database := strings.NewReader(`[
+		{ "Name": "Cleo", "Wins": 10 },
+		{ "Name": "Chris", "Wins": 33 }]`)
+
+		store := FileSystemStore{database}
+
+		got := store.GetPlayerScore("Chris")
+		want := 33
+		assertScoreEquals(t, got, want)
+	})
+}
+
+func assertLeague(t *testing.T, got, want []Player) {
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func assertScoreEquals(t *testing.T, got, want int) {
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
