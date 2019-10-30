b07f39ffc1ae724ce07c388dab3ca29c7676a9d6
huangyao <505757398@163.com>
2019-10-30 21:03:12 +0800

    function

--- function/function.go
+++ function/function.go
@@ -1,1 +1,14 @@
-package function
+package main
+
+import "fmt"
+
+func simple(af func(a, b int) int) {
+	fmt.Println(af(60, 7))
+}
+
+func main() {
+	f := func(a, b int) int {
+		return a + b
+	}
+	simple(f)
+}
