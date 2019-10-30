4bef7ca7ae86af374dd9ddfe38d59021e167001b
huangyao <505757398@163.com>
2019-10-30 21:23:11 +0800

    function final

--- function/function.go
+++ function/function.go
@@ -2,43 +2,18 @@ package main
 
 import "fmt"
 
-type student struct {
-	firstName string
-	lastName  string
-	grade     string
-	country   string
-}
-
-func filter(s []student, f func(student) bool) [] student {
-	var r []student
+func iMap(s []int, f func(int) int) []int{
+	var r []int
 	for _, v := range s {
-		if f(v) == true {
-			r = append(r, v)
-		}
+		r = append(r, f(v))
 	}
 	return r
 }
 
 func main() {
-	s1 := student{
-		firstName: "Naveen",
-		lastName:  "Ramanthan",
-		grade:     "A",
-		country:   "India",
-	}
-	s2 := student{
-		firstName: "Samuel",
-		lastName:  "Johnson",
-		grade:     "B",
-		country:   "USA",
-	}
-
-	s := []student{s1, s2}
-	f := filter(s, func(s student) bool {
-		if s.country == "India" {
-			return true
-		}
-		return false
+	a := []int{5, 6, 7, 8, 9}
+	r := iMap(a, func(n int)int {
+		return n * 5
 	})
-	fmt.Println(f)
+	fmt.Println(r)
 }
