fa9e5bc6ed9fb6aca604eab2239b2689951d177f
huangyao <505757398@163.com>
2019-10-30 21:03:05 +0800

    mutex and channel

--- mutex/mutex.go
+++ mutex/mutex.go
@@ -7,19 +7,22 @@ import (
 
 var x = 0
 
-func increment(wg *sync.WaitGroup, m *sync.Mutex) {
-	m.Lock()
+// using buffered channel with capacity 1
+// channel and mutex 都可以使用，取决于问题对哪个适用
+// 一般在 Goroutines 间需要通信时使用 channels
+func increment(wg *sync.WaitGroup, ch chan bool) {
+	ch <- true
 	x = x + 1
-	m.Unlock()
+	<- ch
 	wg.Done()
 }
 
 func main() {
 	var w sync.WaitGroup
-	var m sync.Mutex
+	ch := make(chan bool, 1)
 	for i := 0; i < 1000; i++ {
 		w.Add(1)
-		go increment(&w, &m)
+		go increment(&w, ch)
 	}
 	w.Wait()
 	fmt.Println("final value of x", x)
